1907cad9c11081e98b3156a8d470300e2d722a2a
Yannick Pereira-Reis <yannick.pereira.reis@gmail.com>
2016-09-29 20:51:49 +0200

    versions and no more setfacl

---
+++ 2.0/script/start.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -o errexit
+#set -x
+
+__DIR__="$(cd "$(dirname "${0}")"; echo $(pwd))"
+__BASE__="$(basename "${0}")"
+__FILE__="${__DIR__}/${__BASE__}"
+
+main () {
+  if [ $APACHE_USER_UID != 0 ];then
+    usermod -u $APACHE_USER_UID $APACHE_RUN_USER
+  fi
+  /usr/sbin/apache2 -D FOREGROUND
+}
+
+main
